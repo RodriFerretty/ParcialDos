0ef24bf404a77f5bb8273311e1c4ed073d8372ba
Rodrigo Ferretty <rodrigoferretty@gmail.com>
2020-06-18 10:13:23 -0300

    Adding components

---
+++ src/app/Components/UserSession/main-session/main-session.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-session',
+  templateUrl: './main-session.component.html',
+  styleUrls: ['./main-session.component.css']
+})
+export class MainSessionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,26 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { MainSessionComponent } from './Components/UserSession/main-session/main-session.component';
+import { RegisterTabComponent } from './Components/UserSession/SubSession/register-tab/register-tab.component';
+import { LoginTabComponent } from './Components/UserSession/SubSession/login-tab/login-tab.component';
+import { CreateLocalComponent } from './Components/Locales/create-local/create-local.component';
+import { CreateProductComponent } from './Components/Products/create-product/create-product.component';
+import { ProductsGridComponent } from './Components/Home/products-grid/products-grid.component';
+import { HomeComponent } from './Components/Home/home/home.component';
+import { MapsComponent } from './Components/Maps/maps/maps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainSessionComponent,
+    RegisterTabComponent,
+    LoginTabComponent,
+    CreateLocalComponent,
+    CreateProductComponent,
+    ProductsGridComponent,
+    HomeComponent,
+    MapsComponent
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
